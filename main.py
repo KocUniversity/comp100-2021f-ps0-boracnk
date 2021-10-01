1cb2b9188b05d4d987d2031b2e421ecd173bc897
boracnk <91727117+boracnk@users.noreply.github.com>
2021-10-01 10:04:56 +0000

    First, I asked the user to enter a number x and a number y. Then I turned them into integers. After that, I printed x to the power of y and log2(x). Lastly, I asked user to enter their ku id number and then printed it.

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Welcome")
+x= input("Please enter number x: ")
+y= input("Please enter number y: ")
+xi= int(x)
+yi= int(y)
+z= xi**yi
+print("Number x raised to the power of y is:")
+print(z)
+print("log(base 2) of x is:")
+import numpy
+print(numpy.log2(xi))
+kuid= input("Enter your KU ID number: ")
+print("Your KU ID number is:")
+print(kuid)
